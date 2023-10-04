ced0ee05eee2936e71ed71d77c988452d51179bd
michael <michael.burzan@outlook.de>
2023-10-04 16:23:24 +0200

    refector moveable

--- core/pmoveable.h
+++ core/pmoveable.h
@@ -2,7 +2,7 @@
 #define PMOVEABLE_H
 #include "pprimitive.h"
 #include "ppoint2d.h"
-#include "pcolider.h"
+#include "../physic/pcollider.h"
 
 #include <optional>
 
@@ -10,12 +10,12 @@ enum class PLineKind{
     VERTICAL,
     HORIZONTAL,
     DIAGONAL
-}
+};
 
 
 class PMoveable{
     public:
-        PMoveable(std::shared_ptr<PDrawable> drawable, std::shared_ptr<PColider> colider) : _drawable(drawable), _colider(colider) {}
+        PMoveable(std::shared_ptr<PDrawable> drawable, std::shared_ptr<PCollider> colider) : _drawable(drawable), _collider(colider) {}
         PMoveable(std::shared_ptr<PDrawable> drawable) : _drawable(drawable) {}
 
 
@@ -26,8 +26,8 @@ class PMoveable{
 
     private:
         auto inpact_on_way(const PPoint2D &v, std::shared_ptr<PDrawable> other) -> std::optional<PPoint2D> {
-            if(_colider){
-                if(_colider->colide(_drawable, other)){
+            if(_collider){
+                if(_collider->collide(_drawable, other)){
                     return PPoint2D(0,0);
                 }
 
@@ -57,13 +57,13 @@ class PMoveable{
                     line_kind = PLineKind::DIAGONAL;
                 }
                 
-
+                //better with binary search
                 switch (line_kind)
                 {
                     case PLineKind::HORIZONTAL:
                         for(float y = y1; y < y2; y += 0.1){
                             _drawable->position(x1,y);
-                            if(_colider->colide(_drawable, other)){
+                            if(_collider->collide(_drawable, other)){
                                 return PPoint2D(x1, y);
                             }
                         }
@@ -71,7 +71,7 @@ class PMoveable{
                     case PLineKind::VERTICAL:
                         for(float x = x1; x < x2; x += 0.1){
                             _drawable->position(x,y1);
-                            if(_colider->colide(_drawable, other)){
+                            if(_collider->collide(_drawable, other)){
                                 return PPoint2D(x, y1);
                             }
                         }
@@ -79,7 +79,7 @@ class PMoveable{
                     case PLineKind::DIAGONAL:
                         for(float x = x1; x < x2; x += 0.1){
                             _drawable->position(x, line_function(x));
-                            if(_colider->colide(_drawable, other)){
+                            if(_collider->collide(_drawable, other)){
                                 return PPoint2D(x, line_function(x));
                             }
                         }
@@ -91,7 +91,7 @@ class PMoveable{
         }
     private:
         std::shared_ptr<PDrawable> _drawable;
-        std::shared_ptr<PColider> _colider;
+        std::shared_ptr<PCollider> _collider;
 };
 
 
